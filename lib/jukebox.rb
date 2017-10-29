7647b91ff2f38d6bb3067ca3b032af5c32c9c76b
github username <github email address>
2017-10-29 19:32:34 -0400

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
@@ -18,43 +16,34 @@ def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
   songs.each_with_index do |song, i|
-    if choice == (i + 1)
+    if choice == (i + 1).to_s || choice == song
       puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
     end
   end
-  # puts "Invalid input, please try again"
 end
 
 def exit_jukebox
   puts "Goodbye"
 end
 
-def run
-
-  songs = [
-    "Phoenix - 1901",
-    "Tokyo Police Club - Wait Up",
-    "Sufjan Stevens - Too Much",
-    "The Naked and the Famous - Young Blood",
-    "(Far From) Home - Tiga",
-    "The Cults - Abducted",
-    "Phoenix - Consolation Prizes",
-    "Harry Chapin - Cats in the Cradle",
-    "Amos Lee - Keep It Loose, Keep It Tight"
-  ]
-
-  until (command == "exit")
+def run(songs)
+  command = ""
+  while command
     help
     puts "Please enter a command:"
     command = gets.chomp
     case command.downcase
-      when "help"
-        help
-      when "list"
-        list(songs)
-      when "play"
-        play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      puts "Goodbye"
+      break
     end
   end
-
 end
